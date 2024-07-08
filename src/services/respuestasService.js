9e329b0cd41caa91166760db2672f3cbcfc22238
santi25789 <138705906+santi25789@users.noreply.github.com>
2024-07-08 19:57:59 -0300

    finished

--- src/services/respuestasService.js
+++ src/services/respuestasService.js
@@ -3,8 +3,8 @@ import RespuestasRepository from "../repositories/respuestasRepository.js"
 const respuestasRepository = new RespuestasRepository();
 
 export default class RespuestasService{
-    async crearRespuesta (user_id, pregunta_id, respuesta_seleccionada, es_respuesta_correcta) {
-        return await respuestasRepository.crearRespuesta(user_id, pregunta_id, respuesta_seleccionada, es_respuesta_correcta);
+    async crearRespuesta (user_id, respuesta_seleccionada, es_respuesta_correcta) {
+        return await respuestasRepository.crearRespuesta(user_id, respuesta_seleccionada, es_respuesta_correcta);
     };
 }
 
