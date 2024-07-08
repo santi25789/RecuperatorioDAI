13689613264e47bdf5c013778f1e576de3c5bb0b
santi25789 <138705906+santi25789@users.noreply.github.com>
2024-07-08 19:58:31 -0300

    finished

--- src/repositories/respuestasRepository.js
+++ src/repositories/respuestasRepository.js
@@ -7,10 +7,11 @@ export default class RespuestasRepository{
         this.DBClient = new Client(DBconfig);
         this.DBClient.connect();   
     }
-    async crearRespuesta (user_id, pregunta_id, respuesta_seleccionada, es_respuesta_correcta) {
+    async crearRespuesta (user_id, respuesta_seleccionada, es_respuesta_correcta) {
         const fecha = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-        const query = 'INSERT INTO Respuestas (user_id, pregunta_id, respuesta_seleccionada, es_respuesta_correcta, fecha_creacion) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-        const values = [user_id, pregunta_id, respuesta_seleccionada, es_respuesta_correcta, fecha];
+        const query = 'INSERT INTO Respuestas (user_id, respuesta_seleccionada, es_respuesta_correcta, fecha_creacion) VALUES ($1, $2, $3, $4) RETURNING *';
+        console.log(query)
+        const values = [user_id, respuesta_seleccionada, es_respuesta_correcta, fecha];
         const { rows } = await this.DBClient.query(query, values);
         return rows[0];
     };
