5afed5462f000bd48714ea41d9cce4f66502e36d
santi25789 <138705906+santi25789@users.noreply.github.com>
2024-07-10 11:32:45 -0300

    Finish

--- src/BD/BD.sql
+++ src/BD/BD.sql
@@ -1,42 +1,42 @@
-CREATE TABLE IF NOT EXISTS public."Respuestas"
+CREATE TABLE IF NOT EXISTS Respuestas
 (
-    "UserId" bigint,
-    "Respuesta_seleccionada" text COLLATE pg_catalog."default",
-    "Es_respuesta_correcta" boolean,
-    "Fecha_creacion" date,
+    UserId bigint,
+    Respuesta_seleccionada text COLLATE pg_catalog."default",
+    Es_respuesta_correcta boolean,
+    Fecha_creacion date,
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999 CACHE 1 )
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."Respuestas"
+ALTER TABLE IF EXISTS Respuestas
     OWNER to postgres;
 
-CREATE TABLE IF NOT EXISTS public."Preguntas"
+CREATE TABLE IF NOT EXISTS Preguntas
 (
-    "Pregunta" text COLLATE pg_catalog."default",
-    "Opcion_1" text COLLATE pg_catalog."default",
-    "Opcion_2" text COLLATE pg_catalog."default",
-    "Opcion_3" text COLLATE pg_catalog."default",
-    "Opcion_4" text COLLATE pg_catalog."default",
-    "Respuesta_correcta" text COLLATE pg_catalog."default",
-    "Fecha_creacion" date,
+    Pregunta text COLLATE pg_catalog."default",
+    Opcion_1 text COLLATE pg_catalog."default",
+    Opcion_2 text COLLATE pg_catalog."default",
+    Opcion_3 text COLLATE pg_catalog."default",
+    Opcion_4 text COLLATE pg_catalog."default",
+    Respuesta_correcta text COLLATE pg_catalog."default",
+    Fecha_creacion date,
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999 CACHE 1 )
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."Preguntas"
+ALTER TABLE IF EXISTS Preguntas
     OWNER to postgres;
 
-INSERT INTO public."Respuestas" ("UserId", "Respuesta_seleccionada", "Es_respuesta_correcta", "Fecha_creacion")
+INSERT INTO Respuestas (UserId, Respuesta_seleccionada, Es_respuesta_correcta, Fecha_creacion)
 VALUES
     ('1', 'Opción A', TRUE, '2024-07-08'),
     ('1', 'Opción B', FALSE, '2024-07-08'),
     ('1', 'Opción C', TRUE, '2024-07-07'),
     ('1', 'Opción D', FALSE, '2024-07-06');
 
-INSERT INTO public."Preguntas" ("Pregunta", "Opcion_1", "Opcion_2", "Opcion_3", "Opcion_4", "Respuesta_correcta", "Fecha_creacion")
+INSERT INTO Preguntas (Pregunta, Opcion_1, Opcion_2, Opcion_3, Opcion_4, Respuesta_correcta, Fecha_creacion)
 VALUES
     ('¿Cuál es la capital de Francia?', 'París', 'Madrid', 'Berlín', 'Londres', 'París', '2024-07-08'),
     ('¿En qué año comenzó la Primera Guerra Mundial?', '1914', '1918', '1939', '1870', '1914', '2024-07-08'),
